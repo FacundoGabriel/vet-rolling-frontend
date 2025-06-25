895291a493f062edf15df093f64385a164b90fe0
Gabriel <gabrielfernandez090997@gmail.com>
2025-06-25 15:40:28 -0300

    Se actualizó el navbar y footer, se subieron img, y se instaló react-icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,18 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./Footer.css";
 import { Link } from "react-router-dom";
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaLinkedin,
+  FaMapMarkerAlt,
+  FaPhoneAlt,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { IoIosMail } from "react-icons/io";
+import logo from "/public/logo.png";
+import senasa from "/public/senasa.png";
+import colegioVet from "/public/colegio-de-vet.png";
 
 const Footer = () => {
   return (
@@ -11,31 +23,44 @@ const Footer = () => {
             sm="12"
             md="6"
             lg="4"
-            className="border border-danger d-flex flex-column align-items-center text-center text-lg-start"
+            className="border border-danger d-flex flex-column align-items-center text-center"
           >
-            <img src="#" alt="img-logo" />
+            <Link>
+              <img src={logo} alt="img-logo" className="logo pt-5" />
+            </Link>
           </Col>
           <Col
             sm="12"
             md="6"
             lg="4"
-            className="border border-danger d-flex flex-column align-items-center text-center text-lg-start"
+            className="border border-danger d-flex flex-column align-items-center text-center text-lg-start pt-3"
           >
             <h5>Veterinaria RollingVet</h5>
-            <ul className="list-unstyled">
+            <ul className="list-unstyled info">
               <li>
+                <Link>
+                  <FaPhoneAlt className="icons-footer" />
+                </Link>
                 <Link to="*">Tel: (0381) 456-7890</Link>
               </li>
               <li>
+                <Link>
+                  <MdPhoneIphone className="icons-footer" />
+                </Link>
                 <Link to="*">Cel: +54 9 381 512-3456</Link>
               </li>
               <li>
-                <Link to="*">Email: rollingvet25@gmail.com</Link>
+                <Link>
+                  <IoIosMail className="icons-footer" />
+                </Link>
+                <Link to="*">rollingvet25@gmail.com</Link>
               </li>
               <li>
+                <Link>
+                  <FaMapMarkerAlt className="icons-footer" />
+                </Link>
                 <Link to="*">
-                  Ubicación: General Paz 576, Piso 9, Oficina 2 San Miguel de
-                  Tucumán
+                  General Paz 576, Piso 9, Oficina 2 San Miguel de Tucumán
                 </Link>
               </li>
             </ul>
@@ -44,31 +69,38 @@ const Footer = () => {
             sm="12"
             md="6"
             lg="4"
-            className="border border-danger d-flex flex-column align-items-center text-center text-lg-start"
+            className="border border-danger d-flex flex-column align-items-center text-center text-lg-start pt-2"
           >
-            <h5>Contacto</h5>
             <ul className="list-unstyled">
-              <li>
-                <Link to="*"></Link>
+              <li className="redes-footer text-center">
+                <Link to="*" target="_blank" rel="noopener noreferrer">
+                  <FaFacebookSquare size={60} />
+                </Link>
+                <Link to="*" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram size={60} />
+                </Link>
+                <Link to="*" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin size={60} />
+                </Link>
               </li>
-              <li>
-                <Link to="*"></Link>
-              </li>
-              <li>
-                <Link to="*"></Link>
-              </li>
-              <li>
-                <Link to="*"></Link>
-              </li>
-              <li>
-                <Link to="*"></Link>
+              <li className="pt-3 text-center">
+                <Link>
+                  <img src={senasa} alt="senasa" className="senasa" />
+                </Link>
+                <Link>
+                  <img
+                    src={colegioVet}
+                    alt="colegio de veterinarios"
+                    className="colegioVet"
+                  />
+                </Link>
               </li>
             </ul>
           </Col>
         </Row>
         <Row className="copyright">
           <Col sm="12" md="12" lg="12" className="text-center py-3 pt-4">
-            <p>&copy; 2025 Ecommerce. Todos los derechos reservados.</p>
+            <p>&copy; 2025 RollingVet. Todos los derechos reservados.</p>
           </Col>
         </Row>
       </Container>
--- src/components/navbar/NavbarC.jsx
+++ src/components/navbar/NavbarC.jsx
@@ -1,33 +1,34 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "./NavbarC.css";
 import { NavLink } from "react-router-dom";
+import logo from "/public/logo.png";
 
 const NavbarC = () => {
   return (
     <>
-      <Navbar expand="lg" className="bg-navbar">
+      <Navbar expand="lg" className="bg-navbar p-5">
         <Container>
           <NavLink className="nav-link">
-            <img src="#" alt="img-logo" width="60" />
+            <img src={logo} alt="img-logo" width="100" />
           </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <NavLink className={"nav-link"} to="/">
+              <NavLink className="nav-link nav-custom" to="/">
                 Inicio
               </NavLink>
-              <NavLink className={"nav-link"} to="/Acerca-De-Nosotros">
+              <NavLink className="nav-link nav-custom" to="/Acerca-De-Nosotros">
                 Acerca de Nosotros
               </NavLink>
-              <NavLink className={"nav-link"} to="/contact">
+              <NavLink className="nav-link nav-custom" to="/contact">
                 Contacto
               </NavLink>
             </Nav>
             <Nav className="ms-auto">
-              <NavLink className="nav-link" to="/login">
+              <NavLink className="nav-link nav-custom" to="/login">
                 Iniciar Sesión
               </NavLink>
-              <NavLink className="nav-link" to="/register">
+              <NavLink className="nav-link nav-custom" to="/register">
                 Registrarse
               </NavLink>
             </Nav>
