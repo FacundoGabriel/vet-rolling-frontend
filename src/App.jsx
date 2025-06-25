64eb78643fe0e88d453f010fbbb029ef1ee5c0d2
Facundo <facundodelacruz01@gmail.com>
2025-06-25 19:41:23 -0300

    se realiza la integracion de el inicio de sesion del usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarC from "./components/navbar/NavbarC";
 import Footer from "./components/footer/Footer";
+import { Login } from "./components/login/Login";
 // import Footer from "./components/footer/Footer";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <NavbarC />
       <Routes>
         <Route />
+          <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
