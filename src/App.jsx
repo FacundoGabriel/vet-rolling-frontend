678bea618ed5bc871197290e081bebe758c1deb1
Alvaro Cordoba <offaim123@gmail.com>
2025-06-25 21:38:59 -0300

    se hace cards para los planes, con la pagina de detalle de plan, y se maneja la funcionalidad de contratar los planes, tambien se hacen las paginas de añadir mascota y mismascotas, y se maneja la funcionalidad de crear, eliminar, editar y obtener mascotas, tambien se cambia el axios.helpers.js para las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarC from "./components/navbar/NavbarC";
 import Footer from "./components/footer/Footer";
+import DetallePlanes from "./pages/DetallePlanes";
+import CardPlanes from "./components/cardplanes/CardPlanes";
+import AniadirMascota from "./pages/AniadirMascota";
+import MisMascotas from "./pages/MisMascotas";
 import { Login } from "./components/login/Login";
 // import Footer from "./components/footer/Footer";
 
@@ -10,7 +14,11 @@ const App = () => {
       <NavbarC />
       <Routes>
         <Route />
-          <Route path="/login" element={<Login />} />
+        <Route path="/" element={<CardPlanes />} />
+        <Route path="/contratar-plan/:id" element={<DetallePlanes />} />
+        <Route path="/mascotas/añadir-mascota" element={<AniadirMascota />} />
+        <Route path="/mascotas/mis-mascotas" element={<MisMascotas />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </Router>
--- src/helpers/axios.helpers.js
+++ src/helpers/axios.helpers.js
@@ -1,16 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
-const token = JSON.parse(sessionStorage.getItem("token"))||null
+const token = JSON.parse(sessionStorage.getItem("token")) || null;
 
 const clientAxios = axios.create({
-    baseURL:`${import.meta.env.VITE_URL_BACK_LOCAL}/api`,
-})
+  baseURL: `${import.meta.env.VITE_URL_BACK_LOCAL}/api`,
+});
 
 export const configHeaders = {
-    headers:{
-        "content-type":"application/json",
-        "auth": `${token}`
-    }
-}
+  headers: {
+    "content-type": "application/json",
+    auth: `${token}`,
+  },
+};
+export const configHeadersImage = {
+  headers: {
+    "content-type": "multipart/form-data",
+  },
+};
 
-export default clientAxios
+export default clientAxios;
