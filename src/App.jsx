1b91d7428f3eaf3b047c4f95f3c9d524cf35569a
Alvaro Cordoba <offaim123@gmail.com>
2025-06-26 15:36:38 -0300

    se cambia algunos colores de los planes y su pagina de detalles, y tambien se pone en su lugar los componentes en el homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import NavbarC from "./components/navbar/NavbarC";
 import Footer from "./components/footer/Footer";
 import HomePage from "./pages/HomePage";
 import DetallePlanes from "./pages/DetallePlanes";
-import CardPlanes from "./components/cardplanes/CardPlanes";
 import AniadirMascota from "./pages/AniadirMascota";
 import MisMascotas from "./pages/MisMascotas";
 import { Login } from "./components/login/Login";
@@ -14,7 +13,6 @@ const App = () => {
       <NavbarC />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/" element={<CardPlanes />} />
         <Route path="/contratar-plan/:id" element={<DetallePlanes />} />
         <Route path="/mascotas/añadir-mascota" element={<AniadirMascota />} />
         <Route path="/mascotas/mis-mascotas" element={<MisMascotas />} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import CarouselC from "../components/carousel/CarouselC";
+import CardPlanes from "../components/cardplanes/CardPlanes";
+import CardVeterinarios from "../components/cardveterinarios/CardVeterinarios";
 
 const HomePage = () => {
   return (
     <>
       <CarouselC />
+      <CardPlanes />
+      <CardVeterinarios />
     </>
   );
 };
