bbdda9fd3a9827f57da33f8229e437f1d0d211f2
Alvaro Cordoba <offaim123@gmail.com>
2025-07-01 16:39:43 -0300

    se añade una tabla para administrar los servicios como admin

---
+++ src/components/tablaservicios/TablaServicios.jsx
@@ -0,0 +1,475 @@
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  Container,
+  Spinner,
+  Image,
+  Button,
+  Badge,
+  Modal,
+  Form,
+} from "react-bootstrap";
+import clientAxios, { configHeadersImage } from "../../helpers/axios.helpers";
+import Swal from "sweetalert2";
+
+const TablaServicios = () => {
+  const [servicios, setServicios] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const [mostrarModalEditar, setMostrarModalEditar] = useState(false);
+  const [idEditando, setIdEditando] = useState(null);
+  const [archivoFoto, setArchivoFoto] = useState(null);
+  const [nuevoServicio, setNuevoServicio] = useState({
+    nombre: "",
+    descripcion: "",
+    precio: "",
+    duracion: "30 minutos",
+    categoria: "Medicina",
+    imagen: "",
+    disponible: true,
+  });
+
+  const eliminarServicio = async (idServicio) => {
+    const confirmacion = await Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Esta acción eliminara el servicio.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Sí, eliminar",
+      cancelButtonText: "No",
+    });
+    if (confirmacion.isConfirmed) {
+      try {
+        const res = await clientAxios.delete(`/servicios/${idServicio}`);
+        Swal.fire("¡Servicio eliminado!", "", "success");
+
+        obtenerServicios();
+      } catch (error) {}
+    }
+  };
+
+  const obtenerServicios = async () => {
+    try {
+      const res = await clientAxios.get(`/servicios`);
+      setServicios(res.data.servicios);
+    } catch (error) {
+      console.error("Error al cargar los servicios:", error);
+    } finally {
+      setCargando(false);
+    }
+  };
+
+  useEffect(() => {
+    obtenerServicios();
+  }, []);
+
+  const handleCambio = (e) => {
+    const { name, value, type, checked } = e.target;
+    setNuevoServicio({
+      ...nuevoServicio,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+  const handleGuardarCambios = async (idServicio) => {
+    try {
+      const res = await clientAxios.put(
+        `/servicios/${idServicio}`,
+        nuevoServicio
+      );
+      Swal.fire("¡Servicio actualizado!", "", "success");
+      setMostrarModalEditar(false);
+      setNuevoServicio({
+        nombre: "",
+        descripcion: "",
+        precio: "",
+        duracion: "30 minutos",
+        categoria: "Medicina",
+        imagen: "",
+        disponible: true,
+      });
+      if (archivoFoto) {
+        const formData = new FormData();
+        formData.append("foto", archivoFoto);
+
+        await clientAxios.put(
+          `/servicios/agregarImagen/${idServicio}`,
+          formData,
+          configHeadersImage
+        );
+      }
+      obtenerServicios();
+    } catch (error) {}
+  };
+
+  const handleGuardar = async () => {
+    try {
+      const res = await clientAxios.post(`/servicios`, nuevoServicio);
+      Swal.fire("¡Servicio creado!", "", "success");
+      setMostrarModal(false);
+      setNuevoServicio({
+        nombre: "",
+        descripcion: "",
+        precio: "",
+        duracion: "30 minutos",
+        categoria: "Medicina",
+        imagen: "",
+        disponible: true,
+      });
+      if (archivoFoto) {
+        const formData = new FormData();
+        formData.append("foto", archivoFoto);
+
+        await clientAxios.put(
+          `/servicios/agregarImagen/${res.data.idServicio}`,
+          formData,
+          configHeadersImage
+        );
+      }
+      obtenerServicios();
+    } catch (error) {
+      console.error("Error al crear el servicio:", error);
+      Swal.fire("Error al crear el servicio", "", "error");
+    }
+  };
+  const limpiarFormularioServicio = () => {
+    setNuevoServicio({
+      nombre: "",
+      descripcion: "",
+      precio: "",
+      duracion: "30 minutos",
+      categoria: "Medicina",
+      imagen: "",
+      disponible: true,
+    });
+  };
+
+  return (
+    <Container className="my-5">
+      <h2 className="mb-4">Administrar Servicios</h2>
+
+      {cargando ? (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "200px" }}
+        >
+          <Spinner animation="border" />
+        </div>
+      ) : (
+        <>
+          <Table
+            striped
+            bordered
+            hover
+            responsive
+            className="align-middle text-center"
+          >
+            <thead className="table-primary">
+              <tr>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Precio</th>
+                <th>Duración</th>
+                <th>Categoría</th>
+                <th>Disponible</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {servicios.map((servicio) => (
+                <tr key={servicio._id}>
+                  <td>
+                    <Image
+                      src={servicio.imagen}
+                      rounded
+                      style={{
+                        width: "80px",
+                        height: "80px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </td>
+                  <td>{servicio.nombre}</td>
+                  <td>{servicio.descripcion}</td>
+                  <td>${servicio.precio.toFixed(2)}</td>
+                  <td>{servicio.duracion}</td>
+                  <td>
+                    <Badge bg="info">{servicio.categoria}</Badge>
+                  </td>
+                  <td>
+                    <Badge bg={servicio.disponible ? "success" : "secondary"}>
+                      {servicio.disponible ? "Sí" : "No"}
+                    </Badge>
+                  </td>
+                  <td>
+                    <Button
+                      variant="warning"
+                      size="sm"
+                      className="me-2"
+                      onClick={() => {
+                        setIdEditando(servicio._id);
+                        setNuevoServicio({
+                          nombre: servicio.nombre,
+                          descripcion: servicio.descripcion,
+                          precio: servicio.precio,
+                          duracion: servicio.duracion,
+                          categoria: servicio.categoria,
+                          imagen: servicio.imagen,
+                          disponible: servicio.disponible,
+                        });
+                        setMostrarModalEditar(true);
+                      }}
+                    >
+                      Editar
+                    </Button>
+                    <Button
+                      variant="danger"
+                      size="sm"
+                      onClick={() => eliminarServicio(servicio._id)}
+                    >
+                      Eliminar
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+
+          <div className="d-flex justify-content-end mt-3">
+            <Button variant="primary" onClick={() => setMostrarModal(true)}>
+              + Añadir Servicio
+            </Button>
+          </div>
+        </>
+      )}
+
+      <Modal show={mostrarModal} onHide={() => setMostrarModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Nuevo Servicio</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-2">
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control
+                type="text"
+                name="nombre"
+                value={nuevoServicio.nombre}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="descripcion"
+                value={nuevoServicio.descripcion}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Precio</Form.Label>
+              <Form.Control
+                type="number"
+                name="precio"
+                value={nuevoServicio.precio}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Duración</Form.Label>
+              <Form.Control
+                type="text"
+                name="duracion"
+                value={nuevoServicio.duracion}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Categoría</Form.Label>
+              <Form.Select
+                name="categoria"
+                value={nuevoServicio.categoria}
+                onChange={handleCambio}
+              >
+                <option value="Medicina">Medicina</option>
+                <option value="Estética">Estética</option>
+                <option value="Estudios">Estudios</option>
+                <option value="Guardería">Guardería</option>
+                <option value="Vacunacion">Vacunación</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mt-3">
+              <Form.Label>Subir imagen</Form.Label>
+              <Form.Control
+                type="file"
+                accept="image/*"
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  const ext = file?.name.split(".").pop().toLowerCase();
+                  if (["jpg", "jpeg", "png"].includes(ext)) {
+                    setArchivoFoto(file);
+                  } else {
+                    Swal.fire({
+                      icon: "error",
+                      title: "Formato de imagen inválido",
+                      text: "Solo se permiten imágenes JPG, JPEG o PNG.",
+                    });
+                    setArchivoFoto(null);
+                    e.target.value = null;
+                  }
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Check
+                type="checkbox"
+                label="Disponible"
+                name="disponible"
+                checked={nuevoServicio.disponible}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setMostrarModal(false)}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={handleGuardar}>
+            Guardar Servicio
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        show={mostrarModalEditar}
+        onHide={() => {
+          setMostrarModalEditar(false);
+          setIdEditando(null);
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Editar Servicio</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-2">
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control
+                type="text"
+                name="nombre"
+                value={nuevoServicio.nombre}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="descripcion"
+                value={nuevoServicio.descripcion}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Precio</Form.Label>
+              <Form.Control
+                type="number"
+                name="precio"
+                value={nuevoServicio.precio}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Duración</Form.Label>
+              <Form.Control
+                type="text"
+                name="duracion"
+                value={nuevoServicio.duracion}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label>Categoría</Form.Label>
+              <Form.Select
+                name="categoria"
+                value={nuevoServicio.categoria}
+                onChange={handleCambio}
+              >
+                <option value="Medicina">Medicina</option>
+                <option value="Estética">Estética</option>
+                <option value="Estudios">Estudios</option>
+                <option value="Guardería">Guardería</option>
+                <option value="Vacunacion">Vacunación</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mt-3">
+              <Form.Label>Cambiar imagen</Form.Label>
+              <Form.Control
+                type="file"
+                accept="image/*"
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  const ext = file?.name.split(".").pop().toLowerCase();
+                  if (["jpg", "jpeg", "png"].includes(ext)) {
+                    setArchivoFoto(file);
+                  } else {
+                    Swal.fire({
+                      icon: "error",
+                      title: "Formato de imagen inválido",
+                      text: "Solo se permiten imágenes JPG, JPEG o PNG.",
+                    });
+                    setArchivoFoto(null);
+                    e.target.value = null;
+                  }
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Check
+                type="checkbox"
+                label="Disponible"
+                name="disponible"
+                checked={nuevoServicio.disponible}
+                onChange={handleCambio}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              setMostrarModalEditar(false);
+              limpiarFormularioServicio();
+            }}
+          >
+            Cancelar
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => handleGuardarCambios(idEditando)}
+          >
+            Guardar Servicio
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
+};
+
+export default TablaServicios;
