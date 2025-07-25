fbbc5b05bcf89203cf62636a7f4853d12aeb15dd
Gabriel <gabrielfernandez090997@gmail.com>
2025-07-25 19:39:19 -0300

    Se modificó la función de pago con mercado pago del carrito

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import TablaProductos from "../components/tablaproductos/TablaProductos";
 import clientAxios, { configHeaders } from "../helpers/axios.helpers";
+import { useLocation } from "react-router-dom";
 
 const Carrito = () => {
   const [productos, setProductos] = useState([]);
+  const location = useLocation();
 
   const obtenerProductoDelCarrito = async () => {
     const res = await clientAxios.get(
@@ -11,11 +13,30 @@ const Carrito = () => {
       configHeaders
     );
     setProductos(res.data.productos);
+    console.log("Productos después de vaciar:", res.data.productos);
   };
 
   useEffect(() => {
-    obtenerProductoDelCarrito();
-  }, []);
+    const procesarPago = async () => {
+      const query = new URLSearchParams(location.search);
+
+      if (query.has("success")) {
+        console.log("Detecté ?success en URL, voy a vaciar carrito");
+        try {
+          await clientAxios.put("/carritos/vaciarCarrito", {}, configHeaders);
+          console.log("Carrito vaciado con éxito, vuelvo a cargar productos");
+          await obtenerProductoDelCarrito();
+        } catch (err) {
+          console.error("Error al vaciar carrito:", err);
+        }
+      } else {
+        // Si no hay success, solo obtenemos los productos normalmente
+        await obtenerProductoDelCarrito();
+      }
+    };
+
+    procesarPago();
+  }, [location.search]);
 
   return (
     <div>
